7dedefc7ddcecb83dd0a329e030d086887c4b564
DrFriendless <friendless.farrell@gmaiil.com>
2016-07-14 12:49:24 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RMud
+
+This is an exercise to learn Ruby on Rails. I intend it to be some sort of rudimentary extensible MUD loosely based on my  great love, LPMud.
---
+++ src/repl.rb
@@ -0,0 +1,84 @@
+require 'highline'
+
+class Response
+  @quit = false
+  @handled = false
+  @message = ()
+
+  def handle()
+    @handled = true
+  end
+
+  def quit()
+    @quit = true
+  end
+
+  def message(s)
+    @message = s
+  end
+
+  def get_message()
+    @message
+  end
+
+  def was_handled()
+    @handled
+  end
+
+  def should_quit()
+    @quit
+  end
+
+  def debug(key)
+    "debug <#{key}> Quit <#{@quit}> handled <#{@handled}> message <#{@message}>"
+  end
+end
+
+class DefaultHandler
+  def handle(response, command)
+    if command == "quit"
+      puts "Command #{command}"
+      response.handle
+      response.quit
+    end
+    if command == "yes"
+      response.handle
+      response.message "Computer says YES"
+    end
+  end
+end
+
+def find_handlers()
+  [ DefaultHandler.new ]
+end
+
+def handle(command)
+  handlers = find_handlers
+  response = Response.new
+  for h in handlers
+    h.handle(response, command)
+    if response.was_handled
+      return response
+    end
+  end
+  response
+end
+
+cli = HighLine.new
+loop do
+  command = cli.ask "> "
+  response = handle(command)
+  if response.should_quit
+    break
+  end
+  if response.was_handled
+    message = response.get_message
+    if message
+      cli.say message
+    else
+      cli.say "<%= color('OK', BOLD) %>"
+    end
+  else
+    cli.say "Computer says NO"
+  end
+end
